289d42ae2126f6827719c11f49d3fc83ea243dba
LeFranck <franco.ac.2020@gmail.com>
2021-03-25 17:01:13 -0300

    Css Tablas y titulos
    Se dejo una vista simple, pero ordenada

--- scripts/static/scripts/index.js
+++ scripts/static/scripts/index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 		await $.ajax({
 			url: 'round_1/',
 			success: function (data) {
-				$('#time_1').text("Esta pregunta tardo " + data["round_1"][1] + " segundos en ser contestada");
+				$('#time_1').text("Esta pregunta tardo " + data["round_1"][1].toString().slice(0, 5) + " segundos en ser contestada");
 				$('#answer_1').text("Aparece " + data["round_1"][0][1] + " veces");
 				$('#answer_2').text("Aparece " + data["round_1"][0][1] + " veces");
 				$('#answer_3').text("Aparece " + data["round_1"][0][2] + " veces");
@@ -26,7 +26,7 @@ $(document).ready(function() {
 			url: 'round_2/',
 			success: function (data) {
 				console.log(data)
-				$('#time_2').text("Esta pregunta tardo " + data["round_2"][1] + " segundos en ser contestada");
+				$('#time_2').text("Esta pregunta tardo " + data["round_2"][1].toString().slice(0, 5) + " segundos en ser contestada");
 				create_episodes_origin_table(data["round_2"][0]);
 			}
 		});
@@ -47,10 +47,14 @@ $(document).ready(function() {
 				str_aux += origin + ", ";
 			});
 			str_origin = str_aux.slice(0, -2) + " ]"
-			episode = '<tr><td><code>episode </code>'+ key +'</td> <td> Cantidad de orígenes distintos: </td></tr>'
-			origins = '<tr><td><code>origins </code>'+ str_origin +'<td>'+ value[0] +'</td></td></tr>'
-			$('#second_round_table tbody').append(episode)
-			$('#second_round_table tbody').append(origins)
+			episode = '<tr><td><code>episode </code>'+ key +'</td> <td> Tiene '+ value[0] +' orígenes distintos</td></tr>'
+			origins = '<tr><td colspan="2"><code>origins </code>'+ str_origin +'</td></tr>'
+			tr = '<tr><td class="col-md-2"><code>episode </code>'+ key +'</td>'
+			tr += '<td class="col-md-2"> Tiene '+ value[0] +' orígenes distintos</td>'
+			tr += '<td class="col-md-8"><code>origins </code>'+ str_origin +'</td></tr>'
+			$('#second_round_table tbody').append(tr)
+			// $('#second_round_table tbody').append(episode)
+			// $('#second_round_table tbody').append(origins)
 		}
 	}
 
